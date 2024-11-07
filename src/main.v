dc9769c1444d05215001b942a055b1216c382a75
prettycoolmat <syahdriandoank@gmail.com>
2024-11-07 21:34:21 +0700

    fmt src/main.v

--- src/main.v
+++ src/main.v
@@ -11,29 +11,33 @@ fn main() {
 	mut i := 0
 	kelemahan := {
 		'gunting': 'batu'
-		'kertas': 'gunting'
-		'batu': 'kertas'
+		'kertas':  'gunting'
+		'batu':    'kertas'
 	}
 	for {
 		i++
 		term.clear()
 		komp := go rand.element(kelemahan.values())
-		println('Ronde $i: Komputer $menangcpu Anda $menanginput')
+		println('Ronde ${i}: Komputer ${menangcpu} Anda ${menanginput}')
 		println('Gunting, batu atau kertas!')
 		input := os.input('Pilih seranganmu! ').to_lower()
-		cpu := komp.wait() or { panic('$err') }
-		println('Serangan mu: $input Serangan komputer: $cpu')
+		cpu := komp.wait() or { panic('${err}') }
+		println('Serangan mu: ${input} Serangan komputer: ${cpu}')
 		match true {
-			input == kelemahan[cpu] { 
-				println(term.green('Kamu menang!')) 
+			input == kelemahan[cpu] {
+				println(term.green('Kamu menang!'))
 				menanginput++
 			}
-			kelemahan[input] == cpu { 
-				println(term.red(r'Yah... kamu kalah. :(')) 
+			kelemahan[input] == cpu {
+				println(term.red(r'Yah... kamu kalah. :('))
 				menangcpu++
 			}
-			input == cpu { println('Kamu seri.') }
-			else { println(term.red('Input invalid!')) }
+			input == cpu {
+				println('Kamu seri.')
+			}
+			else {
+				println(term.red('Input invalid!'))
+			}
 		}
 		time.sleep(1500 * time.millisecond)
 	}
